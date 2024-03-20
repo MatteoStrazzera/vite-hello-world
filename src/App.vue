91d07aa2e33b33a7e2c4d393505496122d3f8c66
MatteoStrazzera <mattestrazz@gmail.com>
2024-03-20 16:04:54 +0100

    first commit + hello world

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style>
+h1 {
+  color: aquamarine;
+}
+</style>
